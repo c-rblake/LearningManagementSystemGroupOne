32b123c238a89aa35729dc543495916347b77b4d
Christoper <c.blake@hotmail.se>
2021-10-06 10:29:02 +0200

    Add project files.

---
+++ LearningManagementSystemGroupOne/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearningManagementSystemGroupOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
